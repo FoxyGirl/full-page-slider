408e19dd5badd9d6f21fb6d6323f98152b696f1c
Elena Pavlova <lenlho22@gmail.com>
2016-10-24 17:36:39 +0300

    Create automatization for FE

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var gulp = require("gulp"),
+    plumber = require("gulp-plumber"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    mqpacker = require("css-mqpacker"),
+    minify = require("gulp-csso"),
+    rename = require("gulp-rename"),
+    server = require("browser-sync").create();
+
+gulp.task("style", function() {
+    gulp.src("css/styles.css")
+        .pipe(plumber())
+        .pipe(postcss([
+            autoprefixer({browsers: [
+                "last 1 version",
+                "last 2 Chrome versions",
+                "last 2 Firefox versions",
+                "last 2 Opera versions",
+                "last 2 Edge versions"
+            ]}),
+            mqpacker({
+                sort: true
+            })
+        ]))
+        //.pipe(gulp.dest("css/"))
+        .pipe(minify())
+        .pipe(rename("styles.min.css"))
+        .pipe(gulp.dest("css/"))
+        .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+    server.init({
+        server: "./",
+        notify: false,
+        open: true,
+        ui: false
+    });
+
+    gulp.watch("css/*.css", ["style"]);
+    gulp.watch("*.html").on("change", server.reload);
+    gulp.watch("js/*.js").on("change", server.reload);
+});
