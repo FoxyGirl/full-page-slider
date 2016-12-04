53c4a9c79b06a9fee17b52643eb4564136726cf0
Elena Pavlova <lenlho22@gmail.com>
2016-12-04 20:48:43 +0300

    Correct CSS for JS slider

--- js/slider_script.js
+++ js/slider_script.js
@@ -7,6 +7,8 @@
         link_6 = document.getElementById('link_6'),
         link_7 = document.getElementById('link_7');
 
+    var MINWIDTH = 960;
+
     /**
      * Move two blocks according direction
      * @param item1 {string} suffix for first block's class
@@ -29,12 +31,31 @@
         document.querySelector('.slider__item--' + item2).classList.remove('move-' + direction);
     }
 
+    function moveSlider(item1, item2, direction) {
+        console.log(this);
+        var windowWidth = document.documentElement.clientWidth;
+        console.log('windowWidth= ' + windowWidth);
+        if (windowWidth > MINWIDTH) {
+            e.preventDefault();
+            move(item1, item2, direction);
+        }
+    }
+
     link_1.addEventListener('click', function (e) {
         console.log(this);
-        e.preventDefault();
-        move('1', '2', 'top');
+        var windowWidth = document.documentElement.clientWidth;
+        console.log('windowWidth= ' + windowWidth);
+        if (windowWidth > MINWIDTH) {
+            e.preventDefault();
+            move('1', '2', 'top');
+        }
     });
 
+/*
+    link_1.addEventListener('click', function (e) {
+        moveSlider('1', '2', 'top');
+    });
+*/
     link_0.addEventListener('click', function (e) {
         console.log(this);
         e.preventDefault();
@@ -43,39 +64,63 @@
 
     link_2.addEventListener('click', function (e) {
         console.log(this);
-        e.preventDefault();
-        move('2', '3', 'right');
+        var windowWidth = document.documentElement.clientWidth;
+        console.log('windowWidth= ' + windowWidth);
+        if (windowWidth > MINWIDTH) {
+            e.preventDefault();
+            move('2', '3', 'right');
+        }
     });
 
     link_5.addEventListener('click', function (e) {
         console.log(this);
-        e.preventDefault();
-        remove('2', '3', 'right');
+        var windowWidth = document.documentElement.clientWidth;
+        console.log('windowWidth= ' + windowWidth);
+        if (windowWidth > MINWIDTH) {
+            e.preventDefault();
+            remove('2', '3', 'right');
+        }
     });
 
     link_4.addEventListener('click', function (e) {
         console.log(this);
-        e.preventDefault();
-        move('2', '4', 'left');
+        var windowWidth = document.documentElement.clientWidth;
+        console.log('windowWidth= ' + windowWidth);
+        if (windowWidth > MINWIDTH) {
+            e.preventDefault();
+            move('2', '4', 'left');
+        }
     });
 
     link_6.addEventListener('click', function (e) {
         console.log(this);
-        e.preventDefault();
-        remove('2', '4', 'left');
+        var windowWidth = document.documentElement.clientWidth;
+        console.log('windowWidth= ' + windowWidth);
+        if (windowWidth > MINWIDTH) {
+            e.preventDefault();
+            remove('2', '4', 'left');
+        }
     });
 
     link_3.addEventListener('click', function (e) {
         console.log(this);
-        e.preventDefault();
-        document.querySelector('.slider__item--2').classList.remove('move-top');
-        move('2', '5', 'top');
+        var windowWidth = document.documentElement.clientWidth;
+        console.log('windowWidth= ' + windowWidth);
+        if (windowWidth > MINWIDTH) {
+            e.preventDefault();
+            document.querySelector('.slider__item--2').classList.remove('move-top');
+            move('2', '5', 'top');
+        }
     });
 
     link_7.addEventListener('click', function (e) {
         console.log(this);
-        e.preventDefault();
-        document.querySelector('.slider__item--2').classList.add('move-top');
-        remove('2', '5', 'top');
+        var windowWidth = document.documentElement.clientWidth;
+        console.log('windowWidth= ' + windowWidth);
+        if (windowWidth > MINWIDTH) {
+            e.preventDefault();
+            document.querySelector('.slider__item--2').classList.add('move-top');
+            remove('2', '5', 'top');
+        }
     });
 })();
